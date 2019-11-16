bf09ea4f6722213a61058a06e36c3ece4fecfc18
mentix02 <manan.yadav02@gmail.com>
2019-11-15 21:05:45 -0800

    removed username param from url in author:update view

--- author/urls.py
+++ author/urls.py
@@ -13,7 +13,7 @@ app_name = 'author'
 urlpatterns = [
     path('', AuthorListAPIView.as_view(), name='list'),
     path('create/', AuthorCreateAPIView.as_view(), name='create'),
-    path('update/<slug:username>/', AuthorUpdateAPIView.as_view(), name='update'),
+    path('update/', AuthorUpdateAPIView.as_view(), name='update'),
     path('detail/<slug:username>/', AuthorDetailAPIView.as_view(), name='detail'),
     path('verify/<uuid:secret_key>/', AuthorAuthenticateAPIView.as_view(), name='verify'),
 ]
