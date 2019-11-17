541bf75661418d12f36592c5a880ef98cb06720a
mentix02 <manan.yadav02@gmail.com>
2019-11-16 19:36:24 -0800

    added a Topic admin with prepopulated slug fields

--- topic/admin.py
+++ topic/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from topic.models import Topic
+
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_on')
+    list_display_links = ('name',)
+    prepopulated_fields = {
+        'slug': ('name', )
+    }
+
+
+admin.site.register(Topic, TopicAdmin)
