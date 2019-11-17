e1a348d9a37599aadd2cc425cf3396ef308dafe7
mentix02 <manan.yadav02@gmail.com>
2019-11-16 16:18:48 -0800

    deprecated slug generator method for Topic names

--- topic/utils.py
+++ topic/utils.py
@@ -1,20 +1,34 @@
+import logging
+
 from django.db.models import QuerySet
 from django.utils.text import slugify
 
 from topic.models import Topic
 
+logger = logging.getLogger(__name__)
+
 
 def generate_slug_for_topic(topic: Topic, new_slug: str = None) -> str:
     """
-    Generates a slug from the name of
-    the Topic instance provided. If a
-    topic from the slug generated is
-    found, append the primary key of the
-    topic found from the queryset and
-    check for that. If that exists too,
-    call itself recursively until a valid
-    unique slug is found - and return it.
+    NOTE - this method is now defunct and is kept for archival
+    purposes since the name and slug field for the Topic model
+    have now been configured with the unique value as True -
+    therefore there's no point in having a complex recursive
+    function just to generate slugs when the names for all Topics
+    will HAVE to be unique.
+
+    --------------------------------------------------------------
+
+    Generates a slug from the name of the Topic instance provided.
+    If a topic from the slug generated is found, append the primary
+    key of the topic found from the queryset and check for that.
+    If that exists too, call itself recursively until a valid unique
+    slug is found - and return it.
     """
+
+    # Log that a defunct function is begin used -
+    logger.warning('Do not use this function as it has been deprecated. '
+                   'Please read more about why in it\'s respective docstring.')
 
     # Create a slug from the topic name provided
     slug = slugify(topic.name)
