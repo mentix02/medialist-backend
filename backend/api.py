b95d35fa2a566cc04aa8ebc190b36e5ac5262643
mentix02 <manan.yadav02@gmail.com>
2019-11-16 17:27:31 -0800

    added topic endpoint to api urls

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = (
+    path('topics/', include('topic.urls')),
     path('authors/', include('author.urls')),
     path('articles/', include('article.urls')),
 )
