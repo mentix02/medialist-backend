65ebc543ebdeb68e04fabf2864320dbca65e2457
mentix02 <manan.yadav02@gmail.com>
2019-11-16 17:25:42 -0800

    Added a TopicDetailSerializer and fixed bug for ListSerializer

--- topic/serializers.py
+++ topic/serializers.py
@@ -1,12 +1,26 @@
 from topic.models import Topic
+from author.serializers import AuthorListSerializer
 
 from rest_framework import serializers
 
 
 class TopicListSerializer(serializers.ModelSerializer):
 
+    author = serializers.StringRelatedField()
+    thumbnail = serializers.URLField(source='get_thumbnail')
     created_on = serializers.DateTimeField(format='%b. %d, %Y')
 
     class Meta:
         model = Topic
-        fields = ('pk', 'name', 'slug', 'created_on', 'uploaded_on'),
+        fields = ('pk', 'name', 'slug', 'created_on', 'author', 'thumbnail')
+
+
+class TopicDetailSerializer(serializers.ModelSerializer):
+
+    author = AuthorListSerializer()
+    thumbnail = serializers.URLField(source='get_thumbnail')
+    created_on = serializers.DateTimeField(format='%b. %d, %Y')
+
+    class Meta:
+        model = Topic
+        exclude = ('thumbnail_url',)
