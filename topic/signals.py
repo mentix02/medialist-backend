4c963b91bb8cd48dc679f088fc915b15ad82f280
mentix02 <manan.yadav02@gmail.com>
2019-11-16 16:19:28 -0800

    removed custom slug generator for post save signal

--- topic/signals.py
+++ topic/signals.py
@@ -1,11 +1,11 @@
 from django.dispatch import receiver
+from django.utils.text import slugify
 from django.db.models.signals import post_save
 
 from topic.models import Topic
-from topic.utils import generate_slug_for_topic
 
 
 # noinspection PyUnusedLocal
 @receiver(post_save, sender=Topic)
 def create_author_secret_key(sender, instance: Topic, **kwargs):
-    slug = generate_slug_for_topic(instance)
+    slug = slugify(instance.name)
