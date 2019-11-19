eb08ac8ea9580457f55f703a647e25fce97e8527
mentix02 <manan.yadav02@gmail.com>
2019-11-19 14:53:12 -0800

    configured urls for views

--- topic/urls.py
+++ topic/urls.py
@@ -5,6 +5,7 @@ from topic.views import (
     TopicDetailAPIView,
     TopicDeleteAPIView,
     TopicCreateAPIView,
+    TopicSortedArticlesAPIView,
 )
 
 app_name = 'topic'
@@ -13,5 +14,6 @@ urlpatterns = [
     path('', TopicListAPIView.as_view(), name='list'),
     path('create/', TopicCreateAPIView.as_view(), name='create'),
     path('delete/<slug:slug>/', TopicDeleteAPIView.as_view(), name='delete'),
-    path('detail/<slug:slug>/', TopicDetailAPIView.as_view(), name='detail')
+    path('detail/<slug:slug>/', TopicDetailAPIView.as_view(), name='detail'),
+    path('detail/<slug:slug>/articles/', TopicSortedArticlesAPIView.as_view(), name='articles')
 ]
