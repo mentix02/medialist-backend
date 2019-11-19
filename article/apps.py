d23f7c0c5959c06c9ae8dbcc3eb31dfe24848f24
mentix02 <manan.yadav02@gmail.com>
2019-11-19 14:55:15 -0800

    added pre_save signal to article's app ready() method

--- article/apps.py
+++ article/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class ArticleConfig(AppConfig):
     name = 'article'
+
+    def ready(self):
+        # noinspection PyUnresolvedReferences
+        from article.signals import generate_article_slug
