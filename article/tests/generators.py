ff7cf773f833921bef53599041a94317d0efaa5e
mentix02 <manan.yadav02@gmail.com>
2019-11-19 15:35:02 -0800

    mode create_article more customizable with kwargs

--- article/tests/generators.py
+++ article/tests/generators.py
@@ -14,12 +14,12 @@ TOPIC_IDS = [topic[0] for topic in Topic.objects.values_list('id')]
 AUTHOR_IDS = [author[0] for author in Author.objects.values_list('id')]
 
 
-def create_article(draft: bool = random.random() < 0.8) -> Article:
+def create_article(draft: bool = random.random() > 0.8, **kwargs) -> Article:
     article = Article.objects.create(
         draft=draft,
         title=fake.text(50).title()[:-1],
-        topic_id=random.choice(TOPIC_IDS),
-        author_id=random.choice(AUTHOR_IDS),
+        topic_id=kwargs.get('topic_id') or random.choice(TOPIC_IDS),
+        author_id=kwargs.get('author_id') or random.choice(AUTHOR_IDS),
         content='\n\n'.join([fake.sentence(170) for _ in range(random.randint(7, 10))]),
     )
     return article
