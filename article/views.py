2d3fa6a72ebce59a7e92ea35f7d00fed6ee27f4e
mentix02 <manan.yadav02@gmail.com>
2019-11-20 12:24:36 -0800

    cleaned up code

--- article/views.py
+++ article/views.py
@@ -6,9 +6,8 @@ from rest_framework.generics import ListAPIView
 
 class RecentArticleListAPIView(ListAPIView):
     """
-    Gets the last N number of articles to
-    display in a list. Used for getting
-    recently written Articles.
+    Gets the last N number of articles to display in a list. Used
+    for getting recently written Articles.
     """
     serializer_class = ArticleListSerializer
 
