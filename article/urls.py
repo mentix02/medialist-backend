f05a2879ecc729b7c9034af4db84449dde93e5a3
mentix02 <manan.yadav02@gmail.com>
2019-11-28 14:51:22 -0800

    configured url for ArticleDetailAPIView

--- article/urls.py
+++ article/urls.py
@@ -1,9 +1,13 @@
-from article.views import RecentArticleListAPIView
-
 from django.urls import path
+
+from article.views import (
+    ArticleDetailAPIView,
+    RecentArticleListAPIView
+)
 
 app_name = 'article'
 
 urlpatterns = (
     path('recent/', RecentArticleListAPIView.as_view(), name='recent'),
+    path('detail/<slug:slug>/', ArticleDetailAPIView.as_view(), name='detail'),
 )
