c99e54063d01bf77956175d3a9120ac4d3dffbe8
mentix02 <manan.yadav02@gmail.com>
2019-11-28 14:51:03 -0800

    added an ArticleDetailAPIView

--- article/views.py
+++ article/views.py
@@ -1,17 +1,28 @@
 from article.models import Article
-from article.serializers import ArticleListSerializer
+from article.serializers import (
+    ArticleListSerializer,
+    ArticleDetailSerializer
+)
 
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 
 class RecentArticleListAPIView(ListAPIView):
     """
     Gets the last N number of articles to display in a list. Used
-    for getting recently written Articles.
+    for getting recently written Articles that aren't drafts.
     """
+    pagination_class = None
     serializer_class = ArticleListSerializer
 
     def get_queryset(self):
         N = self.request.GET.get('N', 12)
         articles = Article.objects.filter(draft=False)[:N]
         return articles
+
+
+class ArticleDetailAPIView(RetrieveAPIView):
+    lookup_field = 'slug'
+    lookup_url_kwarg = 'slug'
+    serializer_class = ArticleDetailSerializer
+    queryset = Article.objects.filter(draft=False)
