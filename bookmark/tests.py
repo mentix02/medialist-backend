e22c7f2d6149498d2591b929e16c27ed7e32b360
mentix02 <manan.yadav02@gmail.com>
2019-11-30 14:28:57 -0800

    wrote unit tests for testing bookmarking articles

--- bookmark/tests.py
+++ bookmark/tests.py
@@ -1,3 +1,77 @@
-from django.test import TestCase
+import random
+import backend.utils as u
+from typing import List
 
-# Create your tests here.
+from django.shortcuts import reverse
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from article.models import Article
+from bookmark.models import Bookmark
+from topic.tests.generators import create_topic
+from author.tests.generators import create_author
+from article.tests.generators import create_article
+
+
+class BookmarkCreationAndDeletionTest(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.author = create_author()
+        cls.topic = create_topic(cls.author.pk)
+        cls.articles: List[Article] = [
+            create_article(topic_id=cls.topic.id, author_id=cls.author.id, draft=False)
+            for _ in range(5)
+        ]
+
+    def test_unauthenticated_bookmark_creation(self):
+
+        article: Article = random.choice(self.articles)
+
+        response = self.client.post(reverse('bookmark:bookmark'), data={
+            'article_id': article.id
+        })
+        data = u.get_json(response)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(data, {
+            'detail': 'Authentication credentials were not provided.'
+        })
+
+    def test_authenticated_bookmark_creation(self):
+        self.client.credentials(HTTP_AUTHORIZATION=u.auth_header(self.author.get_key()))
+        article: Article = random.choice(self.articles)
+
+        self.bookmarked_article_id = article.id
+
+        response = self.client.post(reverse('bookmark:bookmark'), data={
+            'article_id': article.id
+        })
+        data = u.get_json(response)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, msg=data)
+        self.assertEqual(data, {
+            'detail': {
+                'action': 'created'
+            }
+        })
+
+    def test_authenticated_bookmark_deletion(self):
+        self.client.credentials(HTTP_AUTHORIZATION=u.auth_header(self.author.get_key()))
+        bookmark, _ = Bookmark.objects.get_or_create(
+            author=self.author, article=random.choice(self.articles)
+        )
+        bookmarked_article_id = bookmark.article_id
+
+        response = self.client.post(reverse('bookmark:bookmark'), data={
+            'article_id': bookmarked_article_id
+        })
+        data = u.get_json(response)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data, {
+            'detail': {
+                'action': 'deleted'
+            }
+        })
