7f5562a82478dac346fe74473cdbc73ad77e2a4b
mentix02 <manan.yadav02@gmail.com>
2019-11-30 14:27:12 -0800

    added a helper method for formatting token headers

--- backend/utils.py
+++ backend/utils.py
@@ -10,3 +10,7 @@ from rest_framework.response import Response
 def get_json(response: Response) -> typing.Dict[typing.Any, typing.Any]:
     body = response.content.decode()
     return json.loads(body)
+
+
+def auth_header(key: str) -> str:
+    return 'Token {}'.format(key)
