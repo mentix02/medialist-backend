f71dd4df9cc2e11ca5440128dded3c68bd1ec5ca
mentix02 <manan.yadav02@gmail.com>
2019-12-02 00:16:26 -0800

    configured url for ArticleCreateAPIView

--- article/urls.py
+++ article/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 
 from article.views import (
     ArticleDetailAPIView,
+    ArticleCreateAPIView,
     RecentArticleListAPIView
 )
 
 app_name = 'article'
 
 urlpatterns = (
+    path('create/', ArticleCreateAPIView.as_view(), name='create'),
     path('recent/', RecentArticleListAPIView.as_view(), name='recent'),
     path('detail/<slug:slug>/', ArticleDetailAPIView.as_view(), name='detail'),
 )
