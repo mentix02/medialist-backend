b11451fe1fd0390000feea9130f1b8d70381cfd1
mentix02 <manan.yadav02@gmail.com>
2019-12-03 17:33:36 -0800

    added a draft field to admin list view

--- article/admin.py
+++ article/admin.py
@@ -5,7 +5,7 @@ from article.models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display_links = ('title',)
-    list_display = ('id', 'title', 'author', 'created_on')
+    list_display = ('id', 'title', 'author', 'created_on', 'draft')
     prepopulated_fields = {
         'slug': ('title',)
     }
