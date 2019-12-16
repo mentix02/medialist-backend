71272a3b613fdfa34e4efb893cb61e1b90982292
mentix02 <manan.yadav02@gmail.com>
2019-12-16 09:55:14 +0530

    Configured url for ArticlesSortedByTagsAPIView.

--- article/urls.py
+++ article/urls.py
@@ -3,13 +3,15 @@ from django.urls import path
 from article.views import (
     ArticleDetailAPIView,
     ArticleCreateAPIView,
-    RecentArticleListAPIView
+    RecentArticleListAPIView,
+    ArticlesSortedByTagsAPIView
 )
 
 app_name = 'article'
 
 urlpatterns = (
     path('create/', ArticleCreateAPIView.as_view(), name='create'),
+    path('tags/', ArticlesSortedByTagsAPIView.as_view(), name='tags'),
     path('recent/', RecentArticleListAPIView.as_view(), name='recent'),
     path('detail/<slug:slug>/', ArticleDetailAPIView.as_view(), name='detail'),
 )
