a36d731de15e970852dfc9511d08afafb2432aad
mentix02 <manan.yadav02@gmail.com>
2020-03-11 00:07:51 -0700

    Added helperful error message when create_article is called without topics in database.

--- article/tests/generators.py
+++ article/tests/generators.py
@@ -10,6 +10,14 @@ from article.models import Article
 
 fake = faker.Faker()
 
+THUMBNAIL_URL_IDs = (
+    '557',
+    '251',
+    '700',
+    '420',
+    '1000'
+)
+
 TAGS = ['idea', 'good', 'west', 'animal', 'foot', 'for', 'time', 'hello']
 TOPIC_IDS = [topic[0] for topic in Topic.objects.values_list('id')]
 AUTHOR_IDS = [author[0] for author in Author.objects.values_list('id')]
@@ -21,12 +29,16 @@ def create_article(draft: bool = random.random() > 0.93, **kwargs) -> Article:
         nums = random.randint(2, len(TAGS) - 1)
         return list({random.choice(TAGS) for _ in range(nums)})
 
-    article = Article.objects.create(
-        draft=draft,
-        title=fake.text(50).title()[:-1],
-        topic_id=kwargs.get('topic_id') or random.choice(TOPIC_IDS),
-        author_id=kwargs.get('author_id') or random.choice(AUTHOR_IDS),
-        content='\n\n'.join([fake.sentence(170) for _ in range(random.randint(7, 10))]),
-    )
-    article.set_tags_from_string(','.join(_random_tags()))
-    return article
+    try:
+        article = Article.objects.create(
+            draft=draft,
+            title=fake.text(50).title()[:-1],
+            topic_id=kwargs.get('topic_id') or random.choice(TOPIC_IDS),
+            author_id=kwargs.get('author_id') or random.choice(AUTHOR_IDS),
+            content='\n\n'.join([fake.sentence(170) for _ in range(random.randint(7, 10))]),
+        )
+    except IndexError:
+        raise Exception('Create some topics first.')
+    else:
+        article.set_tags_from_string(','.join(_random_tags()))
+        return article
